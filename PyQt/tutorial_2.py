9487ef07391cf799dc8987f25fa311409b484909
josephsurrey <josephsurrey2018@gmail.com>
2024-04-03 14:20:47 +1300

    Update tutorial_2.py

--- PyQt/tutorial_2.py
+++ PyQt/tutorial_2.py
@@ -4,7 +4,7 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 class MainWindow(QMainWindow):
 
-    def __init__(self):
+    """    def __init__(self):
         super().__init__()
 
         self.setWindowTitle("My App")
@@ -21,7 +21,26 @@ class MainWindow(QMainWindow):
         print("Clicked!")
 
     def the_button_was_toggled(self, checked):
-        print("Checked:", checked)
+        print("Checked:", checked)"""
+
+
+def __init__(self):
+    super().__init__()
+
+    self.setWindowTitle("My App")
+
+    self.button = QPushButton("Press Me!")
+    self.button.clicked.connect(self.the_button_was_clicked)
+
+    self.setCentralWidget(self.button)
+
+
+def the_button_was_clicked(self):
+    self.button.setText("You already clicked me.")
+    self.button.setEnabled(False)
+
+    # Also change the window title.
+    self.setWindowTitle("My Oneshot App")
 
 
 app = QApplication(sys.argv)
